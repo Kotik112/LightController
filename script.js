55d6971ef015f5668fb4202f36136d8923e345d6
Arman I <88910492+Kotik112@users.noreply.github.com>
2023-10-23 20:49:35 +0200

    first attempt

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // Toggle power button
 const powerOnButton = document.getElementById("powerOn");
 const powerOffButton = document.getElementById("powerOff");
@@ -12,143 +11,180 @@ let ws;
 let isConnected = false;
 
 document.addEventListener("DOMContentLoaded", () => {
-    connectToWebsocket();
+  connectToWebsocket();
 });
 
 powerOnButton.addEventListener("click", (event) => {
-    if (isConnected) {
-        // Swap background colors
-        powerOnButton.style.backgroundColor = "#f44336";
-        powerOffButton.style.backgroundColor = "#d4d4d4";
-        turnOn(1, 7);
-    }
-    else {
-        console.log("Not connected to websocket");
-        connectToWebsocket();
-    }
+  if (isConnected) {
+    // Swap background colors
+    powerOnButton.style.backgroundColor = "#f44336";
+    powerOffButton.style.backgroundColor = "#d4d4d4";
+    turnOn(1, 7);
+  } else {
+    console.log("Not connected to websocket");
+    connectToWebsocket();
+  }
 });
 
 powerOffButton.addEventListener("click", () => {
-    if (isConnected) {
-        // Swap background colors
-        powerOffButton.style.backgroundColor = "#f44336";
-        powerOnButton.style.backgroundColor = "#d4d4d4";
-        turnOff(1, 7);
-    }
-    else {
-        console.log("Not connected to websocket");
-        connectToWebsocket();
-    }
+  if (isConnected) {
+    // Swap background colors
+    powerOffButton.style.backgroundColor = "#f44336";
+    powerOnButton.style.backgroundColor = "#d4d4d4";
+    turnOff(1, 7);
+  } else {
+    console.log("Not connected to websocket");
+    connectToWebsocket();
+  }
 });
 stPatriksDayButton.addEventListener("click", () => {
-    if (isConnected) {
-        stPatricksDay();
-    }
-    else {
-        console.log("not connected");
-        connectToWebsocket();
-    }
-})
+  if (isConnected) {
+    stPatricksDay();
+  } else {
+    console.log("not connected");
+    connectToWebsocket();
+  }
+});
 
 colorPicker.addEventListener("change", () => {
-    // TODO: Change color of the light bulb
+  // TODO: Change color of the light bulb
 });
 
 christmasButton.addEventListener("click", () => {
-    //console.log(ws.readyState);
-    const universeIndex = 1;
-    const dmxStart = 1
-    const channelCount = 16;
-    if (isConnected) {
-        ws.send(`QLC+API|getChannelsValues|${universeIndex}|${dmxStart}|${channelCount}`);
-    }
-    else {
-        console.log("Not connected to websocket");
-        connectToWebsocket();
-    }
+  //console.log(ws.readyState);
+  const universeIndex = 1;
+  const dmxStart = 1;
+  const channelCount = 16;
+  if (isConnected) {
+    ws.send(
+      `QLC+API|getChannelsValues|${universeIndex}|${dmxStart}|${channelCount}`
+    );
+  } else {
+    console.log("Not connected to websocket");
+    connectToWebsocket();
+  }
 });
 
 const connectToWebsocket = () => {
-    ws = new WebSocket(wsUrl);
+  ws = new WebSocket(wsUrl);
 
-    ws.onopen = () => {
-        isConnected = true;
-        console.log("Connected to websocket");
-    };
+  ws.onopen = () => {
+    isConnected = true;
+    console.log("Connected to websocket");
+  };
 
-    ws.onclose = () => {
-        isConnected = false;
-        console.log("Disconnected from websocket");
-    }
+  ws.onclose = () => {
+    isConnected = false;
+    console.log("Disconnected from websocket");
+  };
 
-    ws.onerror = (error) => {
-        console.log(`Error: ${error}`);
-    };
+  ws.onerror = (error) => {
+    console.log(`Error: ${error}`);
+  };
 
-    ws.onmessage = (event) => {
-        const message = event.data;
-        console.log(`Message: ${message}`);
-        // TODO: Actual logic for handling messages
-    }
-}
+  ws.onmessage = (event) => {
+    const message = event.data;
+    console.log(`Message: ${message}`);
+    // TODO: Actual logic for handling messages
+  };
+};
 
 function sendMessage(message) {
-    if (isConnected) {
-        ws.send(message);
-    }
-    else {
-        alert('Not connected to websocket!');
-        connectToWebsocket();
-    }
+  if (isConnected) {
+    ws.send(message);
+  } else {
+    alert("Not connected to websocket!");
+    connectToWebsocket();
+  }
 }
 
 function turnOn(start, end) {
-    if (start <= 0 || end > 512 || start > end) {
-        alert('Invalid range!');
-        return;
-    }
-    for (start; start < end; start++) {
-        sendMessage(`CH|${start}|255`);
-    }
+  if (start <= 0 || end > 512 || start > end) {
+    alert("Invalid range!");
+    return;
+  }
+  for (start; start < end; start++) {
+    sendMessage(`CH|${start}|255`);
+  }
 }
 
 function turnOff(start, end) {
-    if (start <= 0 || end > 512 || start > end) {
-        alert('Invalid range!');
-        return;
-    }
-    for (start; start < end; start++) {
-        sendMessage(`CH|${start}|0`);
-    }
+  if (start <= 0 || end > 512 || start > end) {
+    alert("Invalid range!");
+    return;
+  }
+  for (start; start < end; start++) {
+    sendMessage(`CH|${start}|0`);
+  }
 }
+
 function stPatricksDay() {
-    let index = 0
-    setInterval(() => {
+  let index = 0;
+  setInterval(() => {
+    const colors = [
+      [255, 95, 0], // Orange
+      [0, 255, 0], // Green
+    ];
 
-        const colors = [
-            [255, 95, 0], // Orange
-            [0, 255, 0], // Green
-        ]
+    let r, g, b;
+    // Lampa 1
+    [r, g, b] = colors[index];
+    ws.send(`CH|1|${r}`);
+    ws.send(`CH|2|${g}`);
+    ws.send(`CH|3|${b}`);
 
-        let r, g, b;
-        // Lampa 1
-        [r, g, b] = colors[index];
-        ws.send(`CH|1|${r}`)
-        ws.send(`CH|2|${g}`)
-        ws.send(`CH|3|${b}`)
+    let secondIndex = index === 1 ? 0 : 1;
 
-        let secondIndex = index === 1 ? 0 : 1;
+    // Lampa 2
+    [r, g, b] = colors[secondIndex];
+    ws.send(`CH|4|${r}`);
+    ws.send(`CH|5|${g}`);
+    ws.send(`CH|6|${b}`);
 
-        // Lampa 2 
-        [r, g, b] = colors[secondIndex];
-        ws.send(`CH|4|${r}`);
-        ws.send(`CH|5|${g}`);
-        ws.send(`CH|6|${b}`);
-
-        index++;
-        if (index >= 2) {
-            index = 0;
-        }
-    }, 1000);
-
+    index++;
+    if (index >= 2) {
+      index = 0;
+    }
+  }, 1000);
 }
+
+const merryGoRound = () => {
+  const RED = 1;
+  const GREEN = 2;
+  const BLUE = 3;
+  const fixtures = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [10, 11, 12],
+    [13, 14, 15],
+    [16, 17, 18],
+    [19, 20, 21],
+    [22, 23, 24],
+    [25, 26, 27],
+    [28, 29, 30],
+  ];
+  let dimmerDown, dimmerUp;
+  const step = 5;
+
+  // (10 lampor * 3 kanaler) + 1 då kanalerna börjar från 1 och inte 0
+  const MAX_CHANNELS = 10 * 3 + 1;
+  setInterval(() => {
+    let counter = 0;
+    let max = 255;
+    let min = 0;
+
+    for (let i = BLUE; i < MAX_CHANNELS; i += 3) {
+      dimmerDown = fixtures[counter];
+      dimmerUp = fixtures[counter + 1];
+      max -= step;
+      min += step;
+      setTimeout(() => {
+        sendMessage(`CH|${dimmerDown[BLUE]}|${max}`);
+        sendMessage(`CH|${dimmerUp[BLUE]}|${min}`);
+      }, 1000);
+    }
+  }, 10_000);
+};
+
+const switchLight = () => {};
